b15234d3a892afa1334c448e11263293fdc04495
Your Name <you@example.com>
2024-02-05 22:38:14 -0800

    Task 5

---
+++ models/engine/file_storage.py
@@ -0,0 +1,16 @@
+import json
+import os
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    """
+
+    """
+    __file_path = "file.json"
+    __objects = {}
+    def new(self.obj):
+        """
+
+        """
+
